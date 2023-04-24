ffa36ac4b56c1bd59564f4dfee151d027a5e966c
rrkeji <rrkeji@163.com>
2023-04-24 11:45:59 +0800

    添加app box

--- plugins/tauri-plugin-rrai-appbox/src/models.rs
+++ plugins/tauri-plugin-rrai-appbox/src/models.rs
@@ -3,8 +3,14 @@ use serde::{Deserialize, Serialize};
 /// 定义 User 类型
 #[derive(Debug, Serialize, Deserialize)]
 struct RraiApplication {
-    name: String,
-    age: i32,
-    gender: String,
-    friends: Vec<String>,
+    pub id: String,
+    pub provider: String,
+    pub application_cid: String,
+    pub application_type: String,
+    pub name: String,
+    pub avatar: String,
+    pub labels: Vec<String>,
+    pub signature: String,
+    pub description: String,
+    pub create_at: u64,
 }
