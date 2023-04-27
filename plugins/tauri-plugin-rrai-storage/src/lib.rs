ea5336172771c173af4e55c9936e21b8f073a1eb
i.xyz <i.xyz@foxmail.com>
2023-04-27 22:16:55 +0800

    add

--- plugins/tauri-plugin-rrai-storage/src/lib.rs
+++ plugins/tauri-plugin-rrai-storage/src/lib.rs
@@ -12,8 +12,28 @@ use tauri::{
 
 /// Initializes the plugin.
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
-    //执行数据库脚本
+    //异步非阻塞
+    tauri::async_runtime::spawn(async move {
+        //启动 ipfs
+        let (mut rx, mut child) = tauri::api::process::Command::new_sidecar("ipfs")
+            .expect("failed to create `ipfs` binary command")
+            .args(["daemon"])
+            .spawn()
+            .expect("Failed to spawn sidecar");
+
+        // read events such as stdout
+        while let Some(event) = rx.recv().await {
+            if let tauri::api::process::CommandEvent::Stdout(line) = event {
+                tracing::debug!("IPFS:{}", line);
+            }
+        }
+    })
+    .expect("执行数据库脚本失败！");
+    //
     tauri::async_runtime::block_on(async move {
+        //判断是否启动 ipfs
+        //判断是否存在配置文件
+
         //执行数据库脚本
         tracing::debug!("执行数据库脚本");
         migration::init_database().await
