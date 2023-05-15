ae65a7844c79030766bd78ee2b3b2e99d74986a7
rrkeji <rrkeji@163.com>
2023-05-15 17:04:31 +0800

    add

--- plugins/tauri-plugin-rrai-ability/src/abilities/mod.rs
+++ plugins/tauri-plugin-rrai-ability/src/abilities/mod.rs
@@ -1,11 +1,11 @@
 mod abilities_service;
 pub mod docker;
+mod perform_service;
 pub mod python;
 mod scan_service;
-mod perform_service;
 pub mod stable_diffusion;
 pub mod stable_diffusion_webui;
 
 pub use abilities_service::*;
-pub use scan_service::*;
 pub use perform_service::*;
+pub use scan_service::*;
