5c795f228291591919673ae63db1087f5fe2693b
rrkeji <rrkeji@163.com>
2023-06-19 09:13:33 +0800

    add

--- plugins/tauri-plugin-rrai-idns/src/constants.rs
+++ plugins/tauri-plugin-rrai-idns/src/constants.rs
@@ -1,3 +1,3 @@
-// pub const RRAI_CLOUD_URL: &str = "https://rrai.idns.link/api";
-pub const RRAI_CLOUD_URL: &str = "http://localhost";
+pub const RRAI_CLOUD_URL: &str = "https://rrai.idns.link/api";
+// pub const RRAI_CLOUD_URL: &str = "http://localhost";
 pub const TOKEN_KEY: &str = "rrai_token";
